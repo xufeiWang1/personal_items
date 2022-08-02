29128b086a0304b95f2df4d8742855543a64e51a
xufeiWang1 <419344705@qq.com>
2022-08-02 21:40:12 +0800

    测试3

--- fairseq-asr/fairseq/tasks/emotion_recognition.py
+++ fairseq-asr/fairseq/tasks/emotion_recognition.py
@@ -3,6 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 #一个注释
+#另一个注释
 import logging
 from pathlib import Path
 from argparse import Namespace
